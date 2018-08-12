21a024774f800423ca95a876922b08196e1df0af
Harshvardhan Sahay <harshvardhansahay5@gmail.com>
2018-08-12 20:16:31 +0530

    Updated XML

--- app/src/main/java/com/personality/mbti/MainActivity.java
+++ app/src/main/java/com/personality/mbti/MainActivity.java
@@ -1,14 +1,20 @@
 package com.personality.mbti;
 
+import android.content.Intent;
 import android.os.Handler;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener{
 
     private TextView appName, subText;
+    GestureDetectorCompat detector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,5 +44,50 @@ public class MainActivity extends AppCompatActivity {
             }
         }, 1000);
 
+        detector = new GestureDetectorCompat(this, this);
+
     }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        if (e1.getX() - e2.getX() > 50) {
+            Intent intent = new Intent(MainActivity.this, questions.class);
+            startActivity(intent);
+            overridePendingTransition(R.anim.enter, R.anim.exit);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        detector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
 }
