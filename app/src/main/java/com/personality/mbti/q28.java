a5c8e10ef97dddd508a636ca3219af228fa07576
Harshvardhan Sahay <harshvardhansahay5@gmail.com>
2018-08-12 23:52:30 +0530

    Layouts of all questions made

---
+++ app/src/main/java/com/personality/mbti/q28.java
@@ -0,0 +1,13 @@
+package com.personality.mbti;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class q28 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_q28);
+    }
+}
